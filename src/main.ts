e6fe8831a5e34151b9536db1374eb79d57ab462d
Raymond Chong <mathsgod@yahoo.com>
2023-10-12 13:03:44 +0800

    🎨 修正(index.html)：新增上傳檔案的功能 📦 新增(uploadFile.ts)：新增上傳檔案的函式 🔧 修正(main.ts)：新增上傳檔案的事件監聽器

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import login from "../lib/login"
 import logout from "../lib/logout"
 import query from "../lib/query"
+import uploadFile from "../lib/uploadFile"
 
 await logout();
 
@@ -10,6 +11,20 @@ console.log(await query({
     my: ["name"]
 }));
 
+document.getElementById("upload").addEventListener("click", async () => {
+    //get file
+    const fileInput = document.getElementById("file") as HTMLInputElement | null;
+    if (!fileInput) {
+        console.error("File input not found");
+        return;
+    }
+    const file = fileInput.files?.[0];
+    if (!file) {
+        console.error("No file selected");
+        return;
+    }
+    //upload file
+    const resp = await uploadFile(file);
 
-
-
+    console.log(resp)
+});
