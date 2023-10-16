1b89d5ef52504b7633b52fb75cd03e908f80b639
Raymond Chong <mathsgod@yahoo.com>
2023-10-16 16:53:04 +0800

    🔧 修正(auth.ts)：新增resetPassword和forgetPassword函數

--- lib/auth.ts
+++ lib/auth.ts
@@ -25,3 +25,17 @@ export const updatePassword = (oldPassword: string, newPassword: string): Promis
     })
 }
 
+
+export const resetPassword = (email: string, password: string, code: string): Promise<boolean> => {
+    return mutation("resetPassword", {
+        email,
+        password,
+        code
+    })
+}
+
+export const forgetPassword = (email: string): Promise<boolean> => {
+    return mutation("forgetPassword", {
+        email
+    })
+}
