f1c4e60c0ebb22c9fb2ae80490fed26d402928ce
Raymond Chong <mathsgod@yahoo.com>
2023-11-08 15:53:05 +0800

    🐛 修正(file.ts)：將字串切片和替換方法改為使用slice和replace函數

--- lib/file.ts
+++ lib/file.ts
@@ -1,12 +1,13 @@
 function decode_base85(a: string) {
     a = "<~" + a + "~>"
-    var c, d, e, f, g, h = String, l = "length", w = 255, x = "charCodeAt", y = "slice", z = "replace";
-    for ("<~" === a[y](0, 2) && "~>" === a[y](-2), a = a[y](2, -2)[z](/\s/g, "")[z]("z", "!!!!!"),
-        c = "uuuuu"[y](a[l] % 5 || 5), a += c, e = [], f = 0, g = a[l]; g > f; f += 5) d = 52200625 * (a[x](f) - 33) + 614125 * (a[x](f + 1) - 33) + 7225 * (a[x](f + 2) - 33) + 85 * (a[x](f + 3) - 33) + (a[x](f + 4) - 33),
+
+    var c, d, e, f, g, h = String, w = 255;
+    for ("<~" === a.slice(0, 2) && "~>" === a.slice(-2), a = a.slice(2, -2).replace(/\s/g, "").replace("z", "!!!!!"),
+        c = "uuuuu".slice(a.length % 5 || 5), a += c, e = [], f = 0, g = a.length; g > f; f += 5) d = 52200625 * (a.charCodeAt(f) - 33) + 614125 * (a.charCodeAt(f + 1) - 33) + 7225 * (a.charCodeAt(f + 2) - 33) + 85 * (a.charCodeAt(f + 3) - 33) + (a.charCodeAt(f + 4) - 33),
             e.push(w & d >> 24, w & d >> 16, w & d >> 8, w & d);
     return function (a, b) {
         for (var c = b; c > 0; c--) a.pop();
-    }(e, c[l]), h.fromCharCode.apply(h, e);
+    }(e, c.length), h.fromCharCode.apply(h, e);
 }
 
 export const file = (s: string = "") => {
