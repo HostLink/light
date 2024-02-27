a61b0aa22e6a5101008fddb691ade1426bd8d035
Raymond Chong <mathsgod@yahoo.com>
2024-02-27 12:15:20 +0800

    🔧 修正(uploadFile.ts)：簡化上傳檔案功能，調整為使用mutation函數進行檔案上傳

--- lib/uploadFile.ts
+++ lib/uploadFile.ts
@@ -1,37 +1,7 @@
-import { jsonToGraphQLQuery, VariableType } from "json-to-graphql-query";
-import { getApiUrl } from '.';
-import { getAxios } from './axios';
-
+import { mutation } from '.';
 
 export default async function (file: File): Promise<any> {
-    const mutation = {
-        __variables: {
-            file: "Upload!"
-        },
-        fsUploadTempFile: {
-            __args: {
-                file: new VariableType("file")
-            },
-            name: true,
-            path: true,
-            size: true,
-            mime: true,
-        }
-    }
-
-    const formData = new FormData();
-    formData.append('operations', JSON.stringify({
-        query: jsonToGraphQLQuery({ mutation })
-    }));
-    formData.append('map', JSON.stringify({ "0": ["variables.file"] }));
-    formData.append('0', file);
-
-    const resp = await getAxios().post(getApiUrl(), formData);
-    if (resp.data.errors) {
-        throw new Error(resp.data.errors[0].message);
-    }
-
-    return resp.data.data.fsUploadTempFile;
-
-
+    return mutation("fsUploadTempFile", {
+        file
+    });
 }
