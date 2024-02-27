0e7c05005d7bba03288fa7edbc1be3d98d1cd1ad
Raymond Chong <mathsgod@yahoo.com>
2024-02-27 12:16:47 +0800

    🔧 修復(uploadFile.ts)：修正上傳檔案函數的參數和回傳值 🚀 新功能(uploadFile.ts)：新增對上傳檔案函數的參數選項設定

--- lib/uploadFile.ts
+++ lib/uploadFile.ts
@@ -1,7 +1,11 @@
 import { mutation } from '.';
-
 export default async function (file: File): Promise<any> {
     return mutation("fsUploadTempFile", {
         file
-    });
+    }, {
+        name: true,
+        path: true,
+        size: true,
+        mime: true,
+    })
 }
