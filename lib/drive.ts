170a55a61653662b35f6cd0e989126b7d1307d34
Raymond Chong <mathsgod@yahoo.com>
2024-11-06 11:34:05 +0800

    🎉 開始專案：新增drive.ts檔案以實現雲端硬碟的功能
    這次提交新增了一個名為`drive.ts`的檔案，該檔案包含了對雲端硬碟的操作功能，包括文件和資料夾的列表、創建、刪除、重命名和移動等操作。這些功能將使應用程式能夠更好地與雲端硬碟進行互動，提升用戶體驗。

---
+++ lib/drive.ts
@@ -0,0 +1,91 @@
+import { AxiosInstance } from 'axios';
+import { mutation, query } from '.';
+
+
+export default (index: number, axios: AxiosInstance) => {
+    return {
+        folders: {
+            list: async (path: string, fields: Object = {
+                name: true,
+                path: true
+            }) => {
+                let resp = await query(axios, {
+                    app: {
+                        drive: {
+                            __args: {
+                                index
+                            },
+                            folders: {
+                                __args: {
+                                    path
+                                },
+                                ...fields
+                            }
+                        },
+                    }
+                });
+
+                return resp.app.drive.folders;
+            },
+            create: (path: string) => {
+                return mutation(axios, "lightDriveCreateFolder", { index, path });
+            },
+            delete: (path: string) => {
+                return mutation(axios, "lightDriveDeleteFolder", { index, path });
+            },
+            rename: (path: string, name: string) => {
+                return mutation(axios, "lightDriveRenameFolder", { index, path, name });
+            }
+        },
+        files: {
+            list: async (path: string, fields: Object = {
+                path: true,
+                size: true,
+                mime: true,
+                canPreview: true
+            }) => {
+                let resp = await query(axios, {
+                    app: {
+                        drive: {
+                            __args: {
+                                index
+                            },
+                            files: {
+                                __args: {
+                                    path
+                                },
+                                ...fields
+                            }
+                        }
+                    }
+                });
+
+                return resp.app.drive.files;
+            },
+            read: async (path: string) => {
+                let resp = await query(axios, {
+                    fsFile: {
+                        __args: {
+                            path
+                        },
+                        base64Content: true,
+                    },
+                });
+
+                return window.atob(resp.fsFile.base64Content);
+            },
+            write: (path: string, content: string) => {
+                return mutation(axios, "lightDriveWriteFile", { index, path, content });
+            },
+            delete: (path: string) => {
+                return mutation(axios, "lightDriveDeleteFile", { index, path });
+            },
+            rename: (path: string, name: string) => {
+                return mutation(axios, "lightDriveRenameFile", { index, path, name });
+            },
+            move: (source: string, target: string) => {
+                return mutation(axios, "lightDriveMoveFile", { index, source, target });
+            }
+        }
+    }
+}
