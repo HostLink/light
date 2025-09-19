404f9062c77adebe46236bf894edbf5d70dfe19a
Raymond Chong <mathsgod@yahoo.com>
2025-09-19 12:17:42 +0800

    ✨ feat(webauthn): 簡化登入與註冊流程，移除不必要的查詢
    * 移除 `query` 函數的使用，直接使用 `useWebAuthn` 提供的 `assertion` 和 `attestation` 方法。
    * 簡化登入和註冊的邏輯，提升代碼可讀性與維護性。

--- lib/webauthn.ts
+++ lib/webauthn.ts
@@ -1,41 +1,22 @@
 import { AxiosInstance } from "axios"
-import { query, mutation } from "."
+import { mutation } from "."
+import useWebAuthn from "./useWebAuthn"
 
 export default (axios: AxiosInstance) => {
+    const { assertion, attestation } = useWebAuthn(axios);
+
     return {
         login: async () => {
-            const { app } = await query(axios, {
-                app: {
-                    auth: {
-                        webAuthnRequestOptions: true
-                    }
-                }
-            })
-
-            const options = app.auth.webAuthnRequestOptions;
-
-            const publicKey = PublicKeyCredential.parseRequestOptionsFromJSON(options);
-            const credential = (await navigator.credentials.get({ publicKey })) as PublicKeyCredential;
+            const credential = await assertion();
             await mutation(axios, "webAuthnAssertion", {
                 assertion: credential.toJSON()
             })
         },
         register: async () => {
-            const { app } = await query(axios, {
-                app: {
-                    auth: {
-                        webAuthnCreationOptions: true
-                    }
-                }
-            });
-
-            const publicKey = PublicKeyCredential.parseCreationOptionsFromJSON(app.auth.webAuthnCreationOptions);
-            const credential = (await navigator.credentials.create({ publicKey })) as PublicKeyCredential;
-
+            const credential = await attestation();
             await mutation(axios, "webAuthnRegister", {
                 registration: credential.toJSON()
             })
-
         }
     }
 }
